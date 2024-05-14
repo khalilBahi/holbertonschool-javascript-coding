581e2b98efaadd3f56f3d06bec169f3f6d13d9a3
khalilBahi <7753@holbertonstudents.com>
2024-05-14 18:01:47 +0200

    remove data

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -7,8 +7,8 @@ const content = process.argv[3];
 
 // Use fs.readFile to read the file content in utf-8
 fs.writeFile(filePath, content, 'utf-8', (error) => {
-    // If an error occurred, print the error object
-    if (error) {
-        console.log(error);
-    }
+  // If an error occurred, print the error object
+  if (error) {
+    console.log(error);
+  }
 });
