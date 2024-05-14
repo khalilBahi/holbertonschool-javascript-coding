a1db16ac8ce9c9219a7270196177bb329605eb41
khalilBahi <7753@holbertonstudents.com>
2024-05-14 17:52:45 +0200

    script that writes a string to a file

---
+++ javascript-web_scraping/1-writeme.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+const fs = require('fs');
+
+// Get the file path from the command line arguments
+const filePath = process.argv[2];
+const content = process.argv[3];
+
+// Use fs.readFile to read the file content in utf-8
+fs.writeFile(filePath, content, 'utf-8', (error, data) => {
+  // If an error occurred, print the error object
+  if (error) {
+    console.log(error);
+  } else {
+    // Print the content of the file
+    console.log(data);
+  }
+});
