244913dd7e55858823eff9099213ddd33ac9a08f
khalilBahi <7753@holbertonstudents.com>
2024-05-23 13:40:50 +0200

    end process when the user sends an interrupt

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,11 +1,14 @@
-console.log('Welcome to Holberton School, what is your name?\n');
-
-// Listen for data from standard input (stdin)
+console.log('Welcome to Holberton School, what is your name?');
 process.stdin.on('data', (data) => {
   // Get the user's input and remove any trailing newline or carriage return characters
   const name = data.toString().trim();
-  console.log(`Your name is: ${name}`);
 
-  console.log('This important software is now closing\n');
+  console.log(`Your name is: ${name}`);
+  process.exit();
+});
+
+// Ensure the process ends when the user sends an interrupt signal (Ctrl+C)
+process.on('SIGINT', () => {
+  console.log('\nThis important software is now closing');
   process.exit();
 });
